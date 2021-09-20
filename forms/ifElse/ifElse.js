9faad4c02787af061c2ef93ca48d4e2f2d0aecf2
LaurenMiranda <laurenmiranda@creighton.edu>
2021-09-19 22:29:47 -0500

    Initial commit

---
+++ forms/ifElse/ifElse.js
@@ -0,0 +1,16 @@
+/*
+let firstName = prompt("Enter your your first name:");
+let State = prompt("Enter your state (two letters capitolized) :");
+let Temp = Number(prompt("Enter the current outside temperature (F):"));
+
+let Message = ['wear a warm coat, hat, scarf and gloves.', 'wear a warm coat but you wont need a hat, scarf or gloves.', 'wear your warmest coat, a warm hat, a scarf, and warm gloves.', 'wear a warm coat, hat and gloves. Maybe a scarf too.']
+
+if (Temp <=32 && State == "NE"){
+  console.log(`${firstName}, ${Message[0]}`)}
+  else if(Temp >=32 && Temp <=50 && State == "NE"){
+   console.log(`${firstName}, ${Message[1]}`)}
+  else if(Temp >=32 && Temp <=50 && State == "FL"){
+    console.log(`${firstName}, ${Message[2]}`)}
+  else if(Temp >=50 && Temp <=70 && State == "FL"){
+     console.log(`${firstName}, ${Message[3]}`)}
+*/
