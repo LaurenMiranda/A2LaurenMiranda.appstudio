e2f78ce8eebd7326b80699ce39c5ec04f01433fc
LaurenMiranda <laurenmiranda@creighton.edu>
2021-09-19 22:53:38 -0500

    Updates

--- forms/Switch/Switch.js
+++ forms/Switch/Switch.js
@@ -1,27 +1,26 @@
 let firstName = prompt("Enter your your first name:");
-let State = prompt("Enter your state (two letters capitolized) :");
+let state = prompt("Enter your state (two letters capitolized) :");
 let Temp = Number(prompt("Enter the current outside temperature (F):"));
 
+let FirstName = firstName.charAt(0).toUpperCase + firstName.slice(1);
+let State= state.toUpperCase();
+let userArray = [FirstName, State, Temp];
+
 let Message = ['wear a warm coat, hat, scarf and gloves.', 'wear a warm coat but you wont need a hat, scarf or gloves.', 'wear your warmest coat, a warm hat, a scarf, and warm gloves.', 'wear a warm coat, hat and gloves. Maybe a scarf too.']
-switch(State) {
-case (State == "NE"):
-    switch(Temp) {
-        case (Temp <=32):
-          console.log(`${firstName}, ${Message[0]}`)
-          break;
-        case (Temp >=32 && Temp <=50):
-          console.log(`${firstName}, ${Message[1]}`)
-          break;
-          }
-case (State == "FL"):
-      switch(Temp) {
-        case Temp >=32 && Temp <=50:
-          console.log(`${firstName}, ${Message[2]}`)
-          break;
-        case Temp >=50 && Temp <=70:
-          console.log(`${firstName}, ${Message[3]}`)
-          break;
-          }
+
+switch(true) {
+case (Temp <32 && State === "NE"):
+  console.log(`${FirstName}, ${Message[0]}`)
+  break;
+case (Temp >=32 && Temp <=50 && State === "NE"):
+  console.log(`${FirstName}, ${Message[1]}`)
+  break;
+case (Temp >=32 && Temp <=50 && State === "FL"):
+  console.log(`${FirstName}, ${Message[2]}`)
+  break;
+case (Temp >50 && Temp <=70 && State === "FL"):
+  console.log(`${FirstName}, ${Message[3]}`)
+  break;
 default:
-  console.log(`Invalid Data`)
+  console.log(`${FirstName}, I do not know what you should wear.`)
     }
--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -1,16 +1,20 @@
 /*
 let firstName = prompt("Enter your your first name:");
-let State = prompt("Enter your state (two letters capitolized) :");
+let state = prompt("Enter your state (two letters capitolized) :");
 let Temp = Number(prompt("Enter the current outside temperature (F):"));
+
+let FirstName = firstName.charAt(0).toUpperCase + firstName.slice(1);
+let State= state.toUpperCase();
+Let userArraay = [FirstName, State, Temp ];
 
 let Message = ['wear a warm coat, hat, scarf and gloves.', 'wear a warm coat but you wont need a hat, scarf or gloves.', 'wear your warmest coat, a warm hat, a scarf, and warm gloves.', 'wear a warm coat, hat and gloves. Maybe a scarf too.']
 
-if (Temp <=32 && State == "NE"){
-  console.log(`${firstName}, ${Message[0]}`)}
-  else if(Temp >=32 && Temp <=50 && State == "NE"){
-   console.log(`${firstName}, ${Message[1]}`)}
-  else if(Temp >=32 && Temp <=50 && State == "FL"){
-    console.log(`${firstName}, ${Message[2]}`)}
-  else if(Temp >=50 && Temp <=70 && State == "FL"){
-     console.log(`${firstName}, ${Message[3]}`)}
+if (Temp <=32 && State === "NE"){
+  console.log(`${FirstName}, ${Message[0]}`)}
+  else if(Temp >=32 && Temp <=50 && State === "NE"){
+   console.log(`${FirstName}, ${Message[1]}`)}
+  else if(Temp >=32 && Temp <=50 && State === "FL"){
+    console.log(`${FirstName}, ${Message[2]}`)}
+  else if(Temp >=50 && Temp <=70 && State === "FL"){
+     console.log(`${FirstName}, ${Message[3]}`)}
 */
